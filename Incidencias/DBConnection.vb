5b58c3047069f0437c38992ea5007069c54907b1
Fernando Martinez <fernando.martinez8b21@gmail.com>
2024-01-16 17:10:55 -0600

    conecion a DB y login con DB

---
+++ Incidencias/DBConnection.vb
@@ -0,0 +1,8 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module DBConnection
+
+    Public conectionSring As String = "Server=192.168.10.3;Database=DBCallCenter;UserId=rootdb;Password=;"
+    Public connection As New MySqlConnection(conectionSring)
+
+End Module
--- Incidencias/Login.vb
+++ Incidencias/Login.vb
@@ -1,4 +1,7 @@
-﻿Public Class Login
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Login
+
 
     Public Class Usuario
         Public Property NombreUsuario As String
@@ -13,6 +16,7 @@
         Usuarios.Add(New Usuario With {.NombreUsuario = "usuario1", .Contrasena = "contraseña1", .IngVentana = "ingventana1"})
         Usuarios.Add(New Usuario With {.NombreUsuario = "usuario2", .Contrasena = "contraseña2", .IngVentana = "ingventana2"})
         ' Puedes agregar más usuarios según sea necesario
+
     End Sub
 
     Private Sub txtUser_TextChanged(sender As Object, e As EventArgs) Handles txtUser.TextChanged
@@ -28,23 +32,52 @@
         Dim nombreUsuario = txtUser.Text
         Dim contrasena = txtPass.Text
 
-        Dim usuario = Usuarios.FirstOrDefault(Function(u) u.NombreUsuario = nombreUsuario AndAlso u.Contrasena = contrasena)
+        'Dim usuario = Usuarios.FirstOrDefault(Function(u) u.NombreUsuario = nombreUsuario AndAlso u.Contrasena = contrasena)
 
-        If usuario IsNot Nothing Then
-            ' Inicio de sesión exitoso
-            AbrirVentana(usuario.IngVentana)
-        Else
-            ' Inicio de sesión fallido
-            MessageBox.Show("Usuario o contraseña incorrectos", "Error de inicio de sesión", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        End If
+        Try
+
+            connection.Open()
+
+            Dim query As String = "SELECT Tipo_Usuario FROM usuario WHERE Nombre=@nombreUsuario AND Pass=@contrasena"
+            Dim cmd As New MySqlCommand(query, connection)
+            cmd.Parameters.AddWithValue("@nombreUsuario", nombreUsuario)
+            cmd.Parameters.AddWithValue("@contrasena", contrasena)
+
+            Dim tipousuario As Object = cmd.ExecuteScalar()
+            'MessageBox.Show(tipousuario)
+
+            Dim reader As MySqlDataReader = cmd.ExecuteReader()
+
+
+            If reader.HasRows Then
+                'MessageBox.Show("Inicio de sesión exitoso")
+                ' Aquí puedes abrir el formulario principal o realizar otras acciones después del inicio de sesión exitoso
+                If tipousuario IsNot Nothing Then
+                    tipousuario.ToString()
+
+                    AbrirVentana(tipousuario)
+                End If
+            Else
+                MessageBox.Show("Credenciales incorrectas")
+            End If
+
+            reader.Close()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            connection.Close()
+
+        End Try
+
     End Sub
 
     Private Sub AbrirVentana(ingVentana As String)
         ' Según el valor de ingVentana, abrir la ventana correspondiente
-        If ingVentana = "ingventana1" Then
+        If ingVentana = "2" Then
             Dim ventana1 As New Form1()
             ventana1.Show()
-        ElseIf ingVentana = "ingventana2" Then
+        ElseIf ingVentana = "1" Then
             Dim ventana2 As New Listainsidentes()
             ventana2.Show()
         End If
